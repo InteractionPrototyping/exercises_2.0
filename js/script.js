254f7a8e764b2d65618301b2487ccd55018fa185
Mariangela Scotto di Carlo <mari.scottodicarlo@gmail.com>
2020-05-19 13:31:18 +0200

    added accordion functionality

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var acc = document.getElementsByClassName("accordion");
+var acc2 = document.getElementsByClassName("active");
+var i;
+var j;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        removeActive();
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+function removeActive () {
+    for (j = 0; j < acc2.length; j++) {
+        var panel2 = acc2[j].nextElementSibling;
+        if(panel2.style.maxHeight) {
+            panel2.style.maxHeight = null;
+            acc2[j].classList.toggle("active");
+        }
+    }
+}
