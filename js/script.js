9aa24c608845289ba63ec92cc0202f808e21c61e
Mariangela Scotto di Carlo <mari.scottodicarlo@gmail.com>
2020-05-19 20:05:29 +0200

    changed structure slightly, took out markSection for now

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
 var acc = document.getElementsByClassName("accordion");
 var acc2 = document.getElementsByClassName("active");
+var acc3 = document.getElementsByClassName("is-active");
 var i;
 var j;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        if (this.className==="accordion") {
-            removeActive();
-        }
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
+function accordion () {
+    /*mark a section
+    for (j = 0; j < acc3.length; j++) {
+        acc3[j].classList.remove("is-active");
+    }
+    var activeSection = document.getElementById(page);
+    activeSection.classList.add("is-active");
+    */
+    /*expand accordion*/
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            if (this.className === "accordion") {
+                removeActive();
+            }
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+                this.classList.remove("active");
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+                this.classList.add("active");
+            }
+        });
+    }
 }
 
 function removeActive () {
@@ -23,7 +35,7 @@ function removeActive () {
         var panel2 = acc2[j].nextElementSibling;
         if(panel2.style.maxHeight) {
             panel2.style.maxHeight = null;
-            acc2[j].classList.toggle("active");
+            acc2[j].classList.remove("active");
         }
     }
 }
