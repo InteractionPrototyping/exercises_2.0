61b5373e2b144baa53a955e3e56795f33b312868
Mariangela Scotto di Carlo <mari.scottodicarlo@gmail.com>
2020-05-19 13:52:58 +0200

    deleted panels from tasks panels close when clicking on other panel

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ var j;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        removeActive();
+        if (this.className==="accordion") {
+            removeActive();
+        }
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
