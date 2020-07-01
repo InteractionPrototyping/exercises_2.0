1aaa9991ea78815d04f40be46b3c049821bd9286
Mariangela Scotto di Carlo <mari.scottodicarlo@gmail.com>
2020-07-01 23:36:15 +0200

    mobile burger menu navigation

--- js/script.js
+++ js/script.js
@@ -58,3 +58,28 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach( el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const menu = document.getElementById("burger_menu");
+                const content = document.getElementById("content");
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                menu.classList.toggle('is-hidden');
+                content.classList.toggle('is-hidden');
+            });
+        });
+    }
+});
