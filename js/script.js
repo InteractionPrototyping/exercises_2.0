db9c46ca17c2eb1c8ffb7bf4b9882938c04d035c
Mariangela Scotto <mari.scottodicarlo@gmail.com>
2020-10-15 13:41:06 +0200

    new spiral pic and Lorenz as responsible person in variables()

--- js/script.js
+++ js/script.js
@@ -133,7 +133,7 @@ function variables() {
     //In Set up: Introduction: "Send your GitHub user names and your group's chosen name to: name_person"
     var el = document.getElementById('name_person');
     if (el != null) {
-        document.getElementById("name_person").innerHTML = "Yucheng Yang.";
+        document.getElementById("name_person").innerHTML = "Lorenz Prasch.";
     } else {
     }
 
@@ -163,41 +163,7 @@ function variables() {
     //Update complete schedule manually in index.html
 }
 
-/*
-$(document).ready(function () {
-    'use strict';
-    // Set up search
-    var index, store;
-    $.getJSON('/lunr.json', function (response) {
-        // Create index
-        index = lunr.Index.load(response.index);
-        // Create store
-        store = response.store;
-        // Handle search
-        $('input#search').on('keyup', function () {
-            // Get query
-            var query = $(this).val();
-            // Search for it
-            var result = index.search(query);
-            // Output it
-            var resultdiv = $('ul.searchresults');
-            if (result.length === 0) {
-                // Hide results
-                resultdiv.hide();
-            } else {
-                // Show results
-                resultdiv.empty();
-                for (var item in result) {
-                    var ref = result[item].ref;
-                    var searchitem = '<li><a href="' + ref + '">' + store[ref].title + '</a></li>';
-                    resultdiv.append(searchitem);
-                }
-                resultdiv.show();
-            }
-        });
-    });
-});
-*/
+
 
 function theme() {
     const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
